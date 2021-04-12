13fe24c3a9dad366edbd233b377fcab9a87272cf
JiarunLiu <jiarunliu@foxmail.com>
2021-04-12 16:52:51 +0800

    first commit

---
+++ train.sh
@@ -0,0 +1,11 @@
+
+for rr in '0.3' '0.6'
+do
+  for onon in '0' '0.25' '0.5' '0.75' '1'
+  do
+    echo "Running r:" ${rr} "on:" ${onon}
+    python Train_cifar.py --r 0.3 --on 0.5 --noisy_dataset cifar100 \
+     --data_path ./../dataset/cifar10/cifar-10-batches-py \
+     --noise_data_dir ./../dataset/cifar100/cifar-100-python
+  done
+done
