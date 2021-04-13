88cba76a12810c33706b3dbbfe21beee8e4a14c6
JiarunLiu <jiarunliu@foxmail.com>
2021-04-13 14:14:12 +0800

    fix bug

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ do
   for onon in '0' '0.25' '0.5' '0.75' '1'
   do
     echo "Running r:" ${rr} "on:" ${onon}
-    python Train_cifar.py --r 0.3 --on 0.5 --noisy_dataset cifar100 \
+    python Train_cifar.py --r ${rr} --on ${onon} --noisy_dataset cifar100 \
      --data_path ./../dataset/cifar10/cifar-10-batches-py \
      --noise_data_dir ./../dataset/cifar100/cifar-100-python
   done
